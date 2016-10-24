9b1186375a897477415e8234aab9d833eb32cb7b
Chui Hin Wah (Dickson) <dicksonZero@gmail.com>
2016-10-24 19:55:01 +0800

    Textured SAM float

---
+++ Assets/dicksonmd/Scripts/Enemies/BSAMRotateStablizers.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BSAMRotateStablizers : MonoBehaviour {
+
+    public float rotSpeed = 3;
+
+    public Transform stablizers;
+	
+	// Update is called once per frame
+	void Update () {
+        this.stablizers.Rotate(Vector3.up, this.rotSpeed * Time.deltaTime);
+
+    }
+}
